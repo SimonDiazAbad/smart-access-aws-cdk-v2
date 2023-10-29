d834bb6df5713605aba92b8be5b00047f26b1df2
Simon Diaz <simon.diaz.abad@gmail.com>
2023-10-28 20:58:36 -0400

    first commit

---
+++ lib/smart-access-aws-cdk-v2-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SmartAccessAwsCdkV2Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // example resource
+    // const queue = new sqs.Queue(this, 'SmartAccessAwsCdkV2Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
